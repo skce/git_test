9def186b9a6c696a6c07c92e4080a60eadfee069
Chaitanya Evani <chaitanya.evani@zenterio.com>
2016-09-22 10:50:39 -0400

    A small change

--- hello.c
+++ hello.c
@@ -32,7 +32,7 @@
 *****************************************************************************/
 int main(int argc, char *argv[])
 {
-   printf("Hello World!");
+   printf("Hello World!\n");
    return 0;
 }
 
